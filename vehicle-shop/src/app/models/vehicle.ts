a866415667be00f0761c54ce04a1e0381672332d
Marno-Warp <marno@warpdevelopment.com>
2025-02-25 12:47:10 +0200

    Added vehicle model and vehicle service with hardcoded data

---
+++ vehicle-shop/src/app/models/vehicle.ts
@@ -0,0 +1,8 @@
+export interface Vehicle {
+    price: number;
+    make: string;
+    model: string;
+    body: string;
+    imageUrl: string;
+  }
+  
---
+++ vehicle-shop/src/app/services/vehicle.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Vehicle } from '../models/vehicle';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class VehicleService {
+    
+  private vehicles: Vehicle[] = [
+    { price: 1000000, make: 'Suzuki', model: 'Swift', body: 'Sedan', imageUrl: '../../assets/images/suzuki_swift.png' },
+    { price: 850000, make: 'Ford', model: 'Figo', body: 'Hatchback', imageUrl: '../../assets/images/ford_figo.webp' },
+    { price: 700000, make: 'Toyota', model: 'Fortuner', body: 'SUV', imageUrl: '../../assets/images/toyota_fortuner.avif' }
+  ];
+
+  vehicles$ = of(this.vehicles);
+
+  getVehicles(): Observable<Vehicle[]> {
+    return this.vehicles$;
+  }
+}
