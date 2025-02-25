4b98b58eb211f5208ceee18a0ccbacaf0906e677
Marno-Warp <marno@warpdevelopment.com>
2025-02-25 12:27:47 +0200

    Creating components

---
+++ vehicle-shop/src/app/components/vehicle-filters/vehicle-filters.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicle-filters',
+  imports: [],
+  templateUrl: './vehicle-filters.component.html',
+  styleUrl: './vehicle-filters.component.scss'
+})
+export class VehicleFiltersComponent {
+
+}
