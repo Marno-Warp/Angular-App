17a17db13face922b4416d0a4140e1064c2021d8
Marno-Warp <marno@warpdevelopment.com>
2025-02-27 13:52:35 +0200

    Created cart component and template

--- vehicle-shop/src/app/app.component.ts
+++ vehicle-shop/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { VehicleListComponent } from './components/vehicle-list/vehicle-list.component';
+import { CartComponent } from './components/cart/cart.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [VehicleListComponent],
+  imports: [VehicleListComponent, CartComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
--- vehicle-shop/src/app/components/cart/cart.component.ts
+++ vehicle-shop/src/app/components/cart/cart.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-cart',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, MatCardModule, MatListModule, MatIconModule, MatButtonModule],
   templateUrl: './cart.component.html',
-  styleUrl: './cart.component.scss'
+  styleUrls: ['./cart.component.scss']
 })
 export class CartComponent {
+  cart$;
 
+  constructor(private cartService: CartService) {
+    this.cart$ = this.cartService.cart$;
+  }
+
+  removeFromCart(vehicleId: number) {
+    this.cartService.removeFromCart(vehicleId);
+  }
+
+  clearCart() {
+    this.cartService.clearCart();
+  }
 }
