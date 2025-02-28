8fd160890cc2acf456a8253283873dc4f4bb053f
Marno-Warp <marno@warpdevelopment.com>
2025-02-28 09:53:58 +0200

    Update app.config.ts

--- vehicle-shop/src/app/app.config.ts
+++ vehicle-shop/src/app/app.config.ts
@@ -7,3 +7,12 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
 };
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBgaWX-Vv7lR69etSumF933mRhIgGAEXoU",
+  authDomain: "vehicle-shop-e2981.firebaseapp.com",
+  projectId: "vehicle-shop-e2981",
+  storageBucket: "vehicle-shop-e2981.firebasestorage.app",
+  messagingSenderId: "373508164027",
+  appId: "1:373508164027:web:490ec40d54eeb0049e1c9e"
+};
