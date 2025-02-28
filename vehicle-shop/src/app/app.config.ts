7c8b76a5e98c68f9c7d2488d1c5ffcb82f1dba2a
Marno-Warp <marno@warpdevelopment.com>
2025-02-28 14:55:54 +0200

    Firebase implementation

--- vehicle-shop/src/app/app.config.ts
+++ vehicle-shop/src/app/app.config.ts
@@ -3,9 +3,11 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({ projectId: "vehicle-shop-e2981", appId: "1:373508164027:web:490ec40d54eeb0049e1c9e", storageBucket: "vehicle-shop-e2981.firebasestorage.app", apiKey: "AIzaSyBgaWX-Vv7lR69etSumF933mRhIgGAEXoU", authDomain: "vehicle-shop-e2981.firebaseapp.com", messagingSenderId: "373508164027" })), provideFirestore(() => getFirestore())]
 };
 
 const firebaseConfig = {
--- vehicle-shop/src/app/services/vehicle.service.ts
+++ vehicle-shop/src/app/services/vehicle.service.ts
@@ -1,25 +1,17 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Vehicle } from '../models/vehicle';
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class VehicleService {
-
-  private vehicles: Vehicle[] = [
-    { id: 1, price: 300000, make: 'Suzuki', model: 'Swift', body: 'Hatchback', imageUrl: 'assets/images/suzuki_swift.png' },
-    { id: 2, price: 550000, make: 'Ford', model: 'Figo', body: 'Sedan', imageUrl: 'assets/images/ford_figo.webp' },
-    { id: 3, price: 1100000, make: 'Toyota', model: 'Fortuner', body: 'SUV', imageUrl: 'assets/images/toyota_fortuner.avif' },
-    { id: 4, price: 3250000, make: 'BMW', model: 'M8', body: 'Sedan', imageUrl: 'assets/images/bmw-m8.webp' },
-    { id: 5, price: 499000, make: 'Mazda', model: 'CX-50', body: 'SUV', imageUrl: 'assets/images/mazda-cx50.png' },
-    { id: 6, price: 269000, make: 'Hyundai', model: 'Grand i10', body: 'Hatchback', imageUrl: 'assets/images/hyundai-grand-i10.webp' },
-  ];
-
-  vehicles$ = of(this.vehicles);
+  private firestore: Firestore = inject(Firestore);
 
   getVehicles(): Observable<Vehicle[]> {
-    return this.vehicles$;
+    const vehiclesCollection = collection(this.firestore, 'vehicles');
+    return collectionData(vehiclesCollection, { idField: 'id' }) as Observable<Vehicle[]>;
   }
 }
